2e8ededa57e4007bd9509347c16977c42c268005
Marcelo Mata <www.escoladeginastica.com>
2025-10-01 18:26:42 -0300

    ajuste para o deploy

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,13 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://gymwodweb.vercel.app", // troque pela sua URL da Vercel
-    ],
+    origin: ["http://localhost:3000", "https://gymwodweb.vercel.app"],
   })
 );
 
-app.get("/health", (_req, res) => res.json({ ok: true }));
+app.get("/health", (_req: express.Request, res: express.Response) => {
+  res.json({ ok: true });
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`API on http://localhost:${port}`));
